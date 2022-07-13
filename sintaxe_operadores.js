5ea6f8df5727c4b1b47e6d567ebd53b479a9a8be
berdfandrade1 <berdfandrade@gmail.com>
2022-07-12 21:59:54 -0300

    baguncinha pra resolver

--- sintaxe_operadores.js
+++ sintaxe_operadores.js
@@ -1,22 +1,25 @@
+// Primeiras variáveis... 
+
 var num1 = "1"
 var num2 = "2"
+
+// transformando as variáveis em integers
 
 parseInt(num1)
 parseInt(num2)
 
+// Criando uma variável para a soma das outras duas variáveis. 
 var soma = num1 + num2
+
+// Função que verifica se os dois números colocados são iguais, ou maior/menor que 10 ou 20 
 
 function log(){
     if (num1 == num2){
         console.log('Os números não podem ser iguais')
-    }else{
-        console.log(soma)
-    }
-
 }
 
-
-if(soma >= 10){
+// consertar esse if statement aqui, mas lembrar de usar o pipe para verificar se um número é maior ou igual a 10/20 etc... 
+if(soma >= 10 || soma <= 20){
     soma = "maior que 10"
 }else if(soma >= 20){
     soma = "maior ou igual a 20"
