533e4ccb6fd5bf4ce83bae10e9eb3f55710f3845
berdfandrade1 <berdfandrade@gmail.com>
2022-07-13 18:23:01 -0300

    Coisas novas no caderno

---
+++ funcao.js
@@ -0,0 +1,12 @@
+var firstName = 'João'
+let lastName = 'Souza'
+
+if(firstName === 'João'){ // Por via de comparação, é bom usar o === 
+    var firstName = 'Pedro';
+    let lastName = 'Silva'; // o lastName não está sendo utilizado... Porque com o let, não estamos usando o nome como valor global. 
+    lastName = 'Benassi'// Eu não posso redeclarar, mas posso reatribuir... Ou seja, quando eu escrevo só lastName = 'Benassi', sem o let, assim acontece a reatribuição...
+
+    console.log(lastName);
+}
+
+console.log(firstName, lastName);
