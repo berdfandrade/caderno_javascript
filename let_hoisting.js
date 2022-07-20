224144edbfc34b1f0f036e3a9cb9db3a4f76c7b5
Bernardo Andrade <96706881+berdfandrade@users.noreply.github.com>
2022-07-20 16:55:26 -0300

    Delete let_hoisting.js

--- let_hoisting.js
+++
@@ -1,7 +0,0 @@
-// Diferente do var, o let precisa ser declarado antes. Então o hoisting só leva o var "para cima..."
-
-let numberOne;
-
-numberOne = 1;
-
-console.log(numberOne + 2) // vai aparecer o 3 
