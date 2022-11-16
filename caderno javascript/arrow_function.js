836fa6c2c91a466498db6815d80822be6624ee92
Bernardo <bernardo@pop-os.localdomain>
2022-11-16 15:30:24 -0300

    Escrevendo no caderno

---
+++ caderno javascript/arrow_function.js
@@ -0,0 +1,10 @@
+// Se antes usaríamos uma funçãoo anônima dessa forma:
+
+const myFunc = function() {
+    const myVar = "value";
+    return myVar;
+}
+
+// Com a arrow function, podemos escrever dessa forma:
+
+const myFunction = () => "Value"; 
